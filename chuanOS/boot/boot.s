7f671db27b07bcc8b868f0bd967fb5c0a287ef90
chuan-ch <1413904352@qq.com>
2025-04-03 20:00:14 +0800

    Update boot.s

--- chuanOS/boot/boot.s
+++ chuanOS/boot/boot.s
@@ -23,10 +23,10 @@ _start:
     xor bx, bx
     
     mov ah, 0x02
-    mov al, 16
-    mov ch, 0
-    mov cl, 2
-    mov dh, 0
+    mov al, 16      ; 读取16个扇区(8KB)
+    mov ch, 0       ; 柱面0
+    mov cl, 2       ; 从扇区2开始(扇区1是MBR)
+    mov dh, 0       ; 磁头0
     mov dl, [boot_drive]
     int 0x13
     jc disk_error
@@ -36,9 +36,9 @@ _start:
     
     ; 获取内存信息
     mov ax, 0xE820
-    mov edx, 0x534D4150
-    mov ecx, 24
-    mov di, _memory_map
+    mov edx, 0x534D4150 ; 'SMAP'
+    mov ecx, 24         ; 缓冲区大小
+    mov di, _memory_map ; 缓冲区地址
     xor ebx, ebx
 
 memory_map_loop:
@@ -67,11 +67,13 @@ disk_error:
     call print_string
     jmp $
 
+; 数据区放在代码之后以减少填充计算误差
 boot_msg db "Booting MyOS...", 0x0D, 0x0A, 0
 error_msg db "Disk Error!", 0x0D, 0x0A, 0
 boot_drive db 0
 _memory_map_count dw 0
 _memory_map times 20*24 db 0
 
-times 510-($-$$) db 0
-dw 0xAA55
+; 确保引导扇区正好512字节
+times 510-($-$$) db 0  ; 填充剩余空间
+dw 0xAA55              ; 引导扇区结束标志
